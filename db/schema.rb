8b20ddb8e57605f6da4ab3c7ecd3cd587baef024
joemecha <jwmecha@gmail.com>
2021-04-11 22:54:41 -0500

    Create migration add description column to bachelorettes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_20_052320) do
+ActiveRecord::Schema.define(version: 2021_04_12_031818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_12_20_052320) do
   create_table "bachelorettes", force: :cascade do |t|
     t.string "name"
     t.integer "season_number"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
